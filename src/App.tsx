9cd64a44f2236767aa506d2182e54c3638b2812e
Ritik <ritikdauthal12345@gmail.com>
2022-11-09 12:03:11 +0530

    Ui

--- src/App.tsx
+++ src/App.tsx
@@ -49,19 +49,19 @@ const handleSearch = () => {
   return (
     <div className="flex-col justify-center items-center " >
       <div className="h-screen">
-      <div className="bg-sky-300 h-1/2 flex justify-center items-center"> 
-      <input type="search" onChange={handleChange} value={city} className="border-4 border-gray-700 rounded-md"/>
-      <button className="px-2 py-1 bg-blue-600 text-white rounded-md" onClick={handleSearch} >Search</button></div>
+      <div className="bg-sky-300 h-1/2 flex flex-col md:flex justify-center items-center "> 
+      <input type="search" placeholder="Enter City Here" onChange={handleChange} value={city} className="border-4 border-gray-700 rounded-md mb-2 md:mb-0 text-center"/>
+      <button className="px-2 py-1 bg-blue-600 text-white rounded-md hover:bg-red-800" onClick={handleSearch} >Search</button></div>
       <div className="h-1/2 bg-gray-300  flex flex-col justify-center items-center">
-        <h1 className="text-3xl font-serif text-black">{data?.name}</h1>
-        <h2 className="text-4xl font-mono text-lime-700 mt-4">{data?.main.temp } kelvin</h2>
-        <div className="flex gap-10"> <h2 className="text-xl font-mono text-red-700">Sunrise - {data?.sys.sunrise}</h2>
-        <span >||</span>
-          <h2 className="text-xl font-mono text-red-700">Sunset - {data?.sys.sunset}</h2>
+        <h1 className="text-3xl font-bold text-black">{data?.name}</h1>
+        <h2 className="md:text-4xl text-lg font-mono text-lime-800 mt-4">{data?.main.temp } kelvin</h2>
+        <div className="flex gap-5 md:gap-10"> <h2 className="md:text-xl text-sm font-mono text-cyan-800">Sunrise - {data?.sys.sunrise}</h2>
+       
+          <h2 className="md:text-xl text-sm font-mono text-cyan-800">Sunset - {data?.sys.sunset}</h2>
           </div>
-           <h2 className="text-xl font-mono text-blue-700">Humidity - {data?.main.humidity}</h2>
-               <h2 className="text-xl font-mono text-blue-700">Pressure - {data?.main.pressure}</h2>
-             <h2 className="text-xl font-mono text-green-700">Wind Speed - {data?.wind.speed}</h2>
+           <h2 className="text-xl font-mono text-green-700">Humidity - {data?.main.humidity}</h2>
+               <h2 className="text-xl font-mono text-yellow-700">Pressure - {data?.main.pressure}</h2>
+             <h2 className="text-xl font-mono text-teal-600">Wind Speed - {data?.wind.speed}</h2>
          </div>
       </div>
      
